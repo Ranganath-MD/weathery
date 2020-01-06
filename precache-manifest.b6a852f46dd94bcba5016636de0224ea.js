924c3c3d032823a3c295f76d43b9e99439758f90
Ranganath-MD <ranganath.developer@gmail.com>
2020-01-06 22:41:36 +0530

    Updates

---
+++ precache-manifest.b6a852f46dd94bcba5016636de0224ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4f4a1478e816a7df74f3700c7f3752",
+    "url": "/weathery/index.html"
+  },
+  {
+    "revision": "686b31d25c9e2df5ec85",
+    "url": "/weathery/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "af442de707d7ecd7cebc",
+    "url": "/weathery/static/css/main.5a714cbe.chunk.css"
+  },
+  {
+    "revision": "686b31d25c9e2df5ec85",
+    "url": "/weathery/static/js/2.78e7ac8a.chunk.js"
+  },
+  {
+    "revision": "af442de707d7ecd7cebc",
+    "url": "/weathery/static/js/main.4cff7406.chunk.js"
+  },
+  {
+    "revision": "88858dd719dcaa29bb75",
+    "url": "/weathery/static/js/runtime-main.d827da2c.js"
+  },
+  {
+    "revision": "ccd3b33385f3a4185819b4231b9e45f7",
+    "url": "/weathery/static/media/w5.ccd3b333.gif"
+  }
+]);
